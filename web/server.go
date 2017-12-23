a82c9426e4be4db1d48dcd68a6eeed2357bdb5a9
omoede.aihe <omoede.aihe@interswitchgroup.com>
2017-12-23 07:29:45 +0100

    added initial code for http server

--- web/server.go
+++ web/server.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/contrib/static"
 	"github.com/oaStuff/cacheServer/server"
+	"net/http"
 )
 
 func StartHttpServer(svr *server.Server) {
@@ -19,6 +20,18 @@ func StartHttpServer(svr *server.Server) {
 	g.Use(static.Serve("/", static.LocalFile("./web", true)))
 	g.NoRoute(func(c *gin.Context) {
 		c.File("./web/index.html")
+	})
+
+	g.POST("/data", func(c *gin.Context) {
+		c.String(http.StatusNoContent,"not yet implemented")
+	})
+
+	g.GET("/data/:key", func(c *gin.Context) {
+		c.String(http.StatusNoContent,"not yet implemented")
+	})
+
+	g.DELETE("/data/:key", func(c *gin.Context) {
+		c.String(http.StatusNoContent,"not yet implemented")
 	})
 
 	g.Run(net.JoinHostPort("",strconv.Itoa(svr.Config.WebPort)))
